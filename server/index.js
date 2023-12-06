df7ecd47dcd2b51ab27f04e4a2724d5f29f88c9d
LuisCode1 <64246172+b4dow@users.noreply.github.com>
2023-12-06 13:41:47 +0100

    Deployando

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-const axios = require('axios')
-const server = require('./src/server')
-const { conn } = require('./src/db.js')
-const { getAllCountriesDB } = require('./src/controllers/countriesController')
-const PORT = 3001
+const axios = require("axios");
+require("dotenv").config();
+const server = require("./src/server");
+const { conn } = require("./src/db.js");
+const { getAllCountriesDB } = require("./src/controllers/countriesController");
+const PORT = process.env || 3001;
 
-conn
-  .sync({ alter: true })
-  .then(() => {
-    server.listen(PORT, () => {
-      console.log(`Server listening on port ${PORT}`)
+conn.sync({ alter: true })
+    .then(() => {
+        server.listen(PORT, () => {
+            console.log(`Server listening on port ${PORT}`);
+        });
     })
-  })
-  .catch(error => console.error(error))
+    .catch((error) => console.error(error));
